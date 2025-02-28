0207a2432b825979a657fae8182c237706b94813
kwakseobang <iii6602@naver.com>
2025-02-28 22:17:13 +0900

    Setting[#1]: build.gardle 의존성 추가

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.kwakmunsu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.27.0")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
