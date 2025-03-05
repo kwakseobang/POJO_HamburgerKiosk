333f6f991a03424de9ada44b64d2fc5c1d45456a
kwakseobang <iii6602@naver.com>
2025-03-06 06:52:04 +0900

    Refactor[#37]: 서비스 구현 로직 도메인 로직으로 이동

--- src/main/java/order/entity/Order.java
+++ src/main/java/order/entity/Order.java
@@ -1,5 +1,9 @@
 package order.entity;
 
+import java.util.List;
+import menu.entity.Menu;
+import menu.response.MenuErrorMessage;
+
 public class Order {
     private String name;
     private long quantity;
@@ -20,6 +24,31 @@ public class Order {
     // 메뉴를 주문한다.
     public void buy() {
 
+    }
+
+    public Menu validateOrderedMenu(List<Menu> menuList) {
+        Menu menu = findByMenu(this.name, menuList);
+        validateQuantity(menu);
+        return menu;
+    }
+
+    private Menu findByMenu(String name, List<Menu> menuList) {
+        return menuList.stream()
+            .filter(menu -> menu.getName().equals(name)).findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(
+                String.format(MenuErrorMessage.NOT_EXIST_MENU.getMessage(), name)
+            ));
+    }
+
+    private void validateQuantity(Menu menu) {
+        int minQuantity = 0;
+        if (menu.isSoldOut()) {
+            throw new RuntimeException(
+                String.format(MenuErrorMessage.INVALID_BUY.getMessage(), menu.getName()));
+        }
+        if (menu.calculateQuantity(this.quantity) < minQuantity ) {
+            throw new IllegalArgumentException(MenuErrorMessage.INVALID_QUANTITY.getMessage());
+        }
     }
 
 }
