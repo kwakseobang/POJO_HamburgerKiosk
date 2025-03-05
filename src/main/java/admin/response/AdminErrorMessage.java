d1e53cc930c419abf8953f9628e938eb14aa0cf7
kwakseobang <iii6602@naver.com>
2025-03-05 17:12:33 +0900

    Fix[#27]: 에러 메세지 형식 수정

--- src/main/java/admin/response/AdminErrorMessage.java
+++ src/main/java/admin/response/AdminErrorMessage.java
@@ -2,8 +2,8 @@ package admin.response;
 
 public enum AdminErrorMessage {
 
-    DUPLICATION_ADMIN("ERROR] - 해당 관리자는 이미 존재합니다: %s"),
-    NOT_EXIST_ADMIN("ERROR] - 존재하지 않는 관리자입니다: %s");
+    DUPLICATION_ADMIN("[ERROR] - 해당 관리자는 이미 존재합니다: %s"),
+    NOT_EXIST_ADMIN("[ERROR] - 존재하지 않는 관리자입니다: %s");
 
     private final String message;
 
