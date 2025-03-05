64ae5546e5f28e644d63215e2c19ec606148dd90
kwakseobang <iii6602@naver.com>
2025-03-06 07:16:05 +0900

    Fix[#39]: return 키워드 추가

--- src/main/java/payment/dto/PaymentCreateDto.java
+++ src/main/java/payment/dto/PaymentCreateDto.java
@@ -1,13 +1,10 @@
 package payment.dto;
 
-import menu.entity.Category;
-import menu.entity.Menu;
-import order.entity.Order;
 import payment.entity.Payment;
 
 public record PaymentCreateDto(String menuName,long menuPrice, long orderQuantity) {
 
     public Payment to() {
-        new Payment(menuName,menuPrice,menuPrice * orderQuantity);
+        return new Payment(menuName, menuPrice, menuPrice * orderQuantity);
     }
 }
