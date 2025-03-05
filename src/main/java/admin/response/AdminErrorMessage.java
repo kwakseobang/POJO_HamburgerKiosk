553c4d3cc74f032fd8c9a0ae3c53211dd12e8665
kwakseobang <iii6602@naver.com>
2025-03-05 19:03:15 +0900

    Feat[#29]: 로그인 에러 메세지 추가

--- src/main/java/admin/response/AdminErrorMessage.java
+++ src/main/java/admin/response/AdminErrorMessage.java
@@ -3,7 +3,9 @@ package admin.response;
 public enum AdminErrorMessage {
 
     DUPLICATION_ADMIN("[ERROR] - 해당 관리자는 이미 존재합니다: %s"),
-    NOT_EXIST_ADMIN("[ERROR] - 존재하지 않는 관리자입니다: %s");
+    NOT_EXIST_ADMIN("[ERROR] - 존재하지 않는 관리자입니다: %s"),
+    UNAUTHORIZED_ADMIN("[ERROR] - 관리자가 로그인하지 않았습니다."),
+    ;
 
     private final String message;
 
