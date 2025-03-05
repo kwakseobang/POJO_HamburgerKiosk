c1ab11535f67965dab266e57d1307df931bb583d
kwakseobang <iii6602@naver.com>
2025-03-05 14:35:06 +0900

    Feat[#23]: 예외 메세지 업데이트

--- src/main/java/menu/response/MenuErrorMessage.java
+++ src/main/java/menu/response/MenuErrorMessage.java
@@ -2,7 +2,8 @@ package menu.response;
 
 public enum MenuErrorMessage {
 
-    INVALID_MENU("[ERROR] - 존재하지 않는 상품입니다: %s"),
+    NOT_EXIST_MENU("[ERROR] - 존재하지 않는 상품입니다: %s"),
+    NOT_EXIST_CATEGORY("[ERROR] - 존재하지 않는 카테고리입니다: %s"),
     INVALID_QUANTITY("[ERROR] - 재고 수량을 초과하여 구매할 수 없습니다."),
     INVALID_BUY("[ERROR] - 품절된 메뉴입니다: %s"),
     ;
