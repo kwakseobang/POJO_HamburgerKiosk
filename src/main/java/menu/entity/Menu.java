e46eb62cb6e2566af38e6cc0284cb05032449fd5
kwakseobang <iii6602@naver.com>
2025-03-05 15:52:40 +0900

    Feat[#23]: 읽어온 파일 메뉴 리스트 저장 기능 구현

--- src/main/java/menu/entity/Menu.java
+++ src/main/java/menu/entity/Menu.java
@@ -2,4 +2,54 @@ package menu.entity;
 
 public class Menu {
 
+    private String name;
+    private long price;
+    private String quantity;
+    private String description;
+    private Category category;
+    private boolean isSoldOut;
+
+    public Menu(String[] parsedMenuItem) {
+        this.name = parsedMenuItem[0];
+        this.price = Long.parseLong(parsedMenuItem[1]);
+        this.quantity = parsedMenuItem[2];
+        this.description = parsedMenuItem[3];
+        this.category = Category.fromString(parsedMenuItem[4]);
+        this.isSoldOut = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category.getName();
+    }
+
+    public boolean isSoldOut() {
+        return isSoldOut;
+    }
+
+    public void updateSoldOut() {
+        this.isSoldOut = true;
+    }
+
+//    public void updateQuantity(String newQuantity) {
+//        String quantity = calculateQuantity(this.quantity, newQuantity);
+//        if (quantity.equals("0")) {
+//            isSoldOut();
+//            this.quantity = "품절";
+//            return;
+//        }
+//        this.quantity = quantity;
+//    }
+
 }
--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -1,5 +1,18 @@
 package menu.repository;
 
+import java.util.List;
+import menu.entity.Menu;
+
 public class MenuRepository {
+
+    private List<Menu> menuList;
+
+    public void save(Menu menu) {
+        menuList.add(menu);
+    }
+
+    public List<Menu> readMenuList() {
+        return menuList;
+    }
 
 }
