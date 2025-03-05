0029143684965f9c792a0927c98916175bd83d6e
kwakseobang <iii6602@naver.com>
2025-03-05 17:12:57 +0900

    Feat[#27]: 잔액 부족 예외 메세지 추가

--- src/main/java/customer/response/CustomerErrorMessage.java
+++ src/main/java/customer/response/CustomerErrorMessage.java
@@ -2,8 +2,10 @@ package customer.response;
 
 public enum CustomerErrorMessage {
 
-    DUPLICATION_CUSTOMER("해당 회원은 이미 존재합니다: %s"),
-    NOT_EXIST_CUSTOMER("존재하지 않는 회원입니다: %s");
+    DUPLICATION_CUSTOMER("[ERROR] - 해당 회원은 이미 존재합니다: %s"),
+    NOT_EXIST_CUSTOMER("[ERROR] - 존재하지 않는 회원입니다: %s"),
+    NOT_ENOUGH_MONEY("[ERROR] - 잔액이 부족합니다."),
+    ;
 
     private final String message;
 
